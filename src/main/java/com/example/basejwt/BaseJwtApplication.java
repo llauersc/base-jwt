6653ed0bef1aa0807281b3fdb8b8e27a84c3bf27
DESKTOP-VM7P796\admin <rustam.gazizov@bpium.ru>
2022-08-10 16:13:00 +0300

    initial commit

---
+++ src/main/java/com/example/basejwt/BaseJwtApplication.java
@@ -0,0 +1,12 @@
+package com.example.basejwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseJwtApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/basejwt/BaseJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basejwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
